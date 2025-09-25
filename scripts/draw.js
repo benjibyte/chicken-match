dde78de7a6f5d89085d366b71ab64d05086544b5
benjibyte <benjipixel@proton.me>
2025-09-25 18:21:45 -0500

    adding some sprites. testing rendering

--- scripts/draw.js
+++ scripts/draw.js
@@ -1,16 +1,17 @@
-function drawImageToCanvas() {
-    let canvas = document.getElementById("c");
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d"); // context = 2D dimension
 
-    let ctx = canvas.getContext("2d"); // The ctx means "context." It gives us access to all the drawing methods that we need for our Canvas ellement.
+const chicken = new Image();
+chicken = document.getElementById("whiteChicken1");
 
-    const barn = new Image();
-    barn.src = "/images/barn.jpg";
-    barn.onload = function() {  
-        ctx.drawImage(barn, 0, 0, 128,128);
-    }
 
-}
 
-document.addEventListener("DOMContentLoaded", function() {
-    drawImageToCanvas();
-});
+let windowHeight = window.innerHeight;
+let windowWidth = window.innerWidth;
+
+canvas.width = windowWidth;
+canvas.height = windowHeight;
+
+canvas.style.background = "#1e032eff";
+
+context.drawImage(chicken, 100, 100);
