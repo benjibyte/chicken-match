0e3533d3e2d32aa9e77d0f6a320f838f089f25b5
benjibyte <benjipixel@proton.me>
2025-09-25 16:58:33 -0500

    workign on rendering the pixel art in

--- scripts/draw.js
+++ scripts/draw.js
@@ -1,5 +1,16 @@
 function drawImageToCanvas() {
-    let image = document.querySelector("img");
-    let canvas = document.querySelector("canvas");
-    
+    let canvas = document.getElementById("c");
+
+    let ctx = canvas.getContext("2d"); // The ctx means "context." It gives us access to all the drawing methods that we need for our Canvas ellement.
+
+    const barn = new Image();
+    barn.src = "/images/barn.jpg";
+    barn.onload = function() {  
+        ctx.drawImage(barn, 0, 0, 128,128);
+    }
+
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    drawImageToCanvas();
+});
