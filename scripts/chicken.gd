a4bda76cd9b0b784610b5370bc737d68a58599a3
benjibyte <benjipixel@proton.me>
2025-12-20 15:26:12 -0600

    began implementation of egg color, egg spawning, global variables, and signals

--- scripts/chicken.gd
+++ scripts/chicken.gd
@@ -2,10 +2,18 @@ extends Area2D
 
 @onready var _animated_sprite = $AnimatedSprite2D
 
+var colors = ["white", "brown", "blue"]
+var egg_color := "not chosen yet"
+
+func spawn_egg(color):
+	pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# Set the Initial Chicken State
+	# Decide what color of egg to lay.
+	color = randi_range(0,2)
+	egg_color = colors[color]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,6 +22,7 @@ func _process(delta: float) -> void:
 
 func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
 	print("click!")
+	spawn_egg(
 	# spawn egg scene
 	# play the poof animation
 	# set the global "state" variable to "waiting."
