963c7c6155759aa93dc44b5416d5e0590e78678b
benjibyte <benjipixel@proton.me>
2025-12-20 13:54:11 -0600

    setting up some global variables

--- game.gd
+++ game.gd
@@ -1,5 +1,12 @@
 extends Node
 
+const egg_max := 9
+var found_eggs := 0
+var chicken_states = [
+	"idle", # Just existing. Hasn't been tapped/clicked yet.
+	"hidden", # a match has occured. Once state has been recieved, butcher the chicken.
+	"waiting" # Recently been tapped, has played the poof animation, and is hidden until 3 chickens has been tapped
+	]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
