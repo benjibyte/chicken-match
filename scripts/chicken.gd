09189acaf959b3b831471b51d1c6a93d86ad3354
benjibyte <benjipixel@proton.me>
2025-12-20 13:46:35 -0600

    Edited some animations; Figured out how to Build the scene using Area2D nodes

---
+++ scripts/chicken.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@onready var _animated_sprite = $AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	# tap or click on the chicken triggers POOF
+	# POOF: means to spawn an egg and play the poof animation.
+	
+	
