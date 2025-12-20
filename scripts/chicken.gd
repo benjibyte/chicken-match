2862d9eef710639beb9af1c422bc48665e658c13
benjibyte <benjipixel@proton.me>
2025-12-20 17:09:41 -0600

    Crackin' Eggs!

--- scripts/chicken.gd
+++ scripts/chicken.gd
@@ -9,16 +9,20 @@ var egg_color := "not chosen yet"
 var local_state = game.chicken_states[0] # 0 = "idle", 1 = "waiting" 3 = "dead"
 
 func spawn_egg(color):
+	var new_egg = 
 	print("Made an " + color + " egg!")
-	pass
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Set the Initial Chicken State
-	local_state = "idle"
+	local_state = "idle"\
+	
+	# Prepare Eggs
 	# Decide what color of egg to lay.
 	var color_index = randi_range(0,2)
 	egg_color = colors[color_index]
+	var eggs = preload("res://egg.tscn")
 
 	# make sure we don't have more than 3 of the same egg.
 	if egg_color == "brown" and game.brown_eggs < 3:
@@ -44,7 +48,7 @@ func _process(_delta: float) -> void:
 
 func _input_event(_viewport: Viewport, _event: InputEvent, _shape_idx: int) -> void:
 	# Only Click and Touch events are valid
-	if (_event is InputEventMouseButton and _event.pressed) and (_event.button_index == MOUSE_BUTTON_LEFT):
+	if ((_event is InputEventMouseButton and _event.pressed) and (_event.button_index == MOUSE_BUTTON_LEFT)) or (_event is InputEventScreenTouch and _event.pressed):
 		if game.chickens_selected < 3:
 			emit_signal("chicken_chosen")
 			# change local_state to be read by game.gd
